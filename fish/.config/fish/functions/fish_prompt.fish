ad3fe68f8a0e8b42cb2fffa5d8403ce3b4b0f13e
Noah Biederbeck <nbiederbeck@tutanota.com>
2019-10-07 14:47:16 +0200

    nicer fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -3,20 +3,16 @@ function fish_prompt
     if not set -q VIRTUAL_ENV_DISABLE_PROMPT
         set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
-    set_color yellow
-    printf '%s' (whoami)
-    set_color normal
 
-    # printf ' at '
-    # set_color magenta
-    # echo -n (prompt_hostname)
-    # set_color normal
-
-    printf ' in '
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
-    set_color normal
 
+    # set_color blue
+    # printf '%s' (__fish_git_prompt)
+    # set_color red
+    # printf '%s' (__fish_git_prompt_informative_status)
+
+    set_color normal
     # Line 2
     echo
     if test $VIRTUAL_ENV
