fa70ad64addfffe96100f317791776f5b0c44cef
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-01-07 13:54:00 +0100

    use single bottom polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,8 +4,9 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar top &!
-polybar bottom &!
+# polybar top &!
+# polybar bottom &!
 # polybar combined &!
+polybar bottom_only &!
 
 echo "Bars launched"
