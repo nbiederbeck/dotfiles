a69cd421f0a1187dac26f0409714aa9f3534e193
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-01-09 13:55:55 +0100

    add an autolock

--- i3/.config/i3lock/lock_when_idle.sh
+++ i3/.config/i3lock/lock_when_idle.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 xidlehook \
-    --not-when-audio --not-when-fullscreen \
-    --notify 5 \
-    --notifier "notify-send --urgency=critical --expire-time=5000 \
-                'locking in 5s'" \
-    --canceller "notify-send --urgency=low --expire-time=5000 \
-                'lock cancelled'" \
-    --timer "betterlockscreen -l dimblur" \
-    --time 2
+    --timer 60 "i3lock -i ${HOME}/Pictures/blue_screen_of_death_resize.jpg -t" "" \
+    --not-when-audio --not-when-fullscreen
