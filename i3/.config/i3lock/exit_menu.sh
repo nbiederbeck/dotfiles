f8f116f757eba6fe9de00583e00d80df432701e0
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-02-02 17:23:43 +0100

    cleaner exit menu with dmenu

--- i3/.config/i3lock/exit_menu.sh
+++ i3/.config/i3lock/exit_menu.sh
@@ -4,11 +4,6 @@ LOCK="i3lock --blur=0.2"
 SLEEP="i3lock -i ${HOME}/Pictures/background && systemctl suspend"
 GIT="~/.local/dotbin/checkgit"
 
-ROFI_TEXT="Exit"
-ROFI_OPTIONS=(-width 100 -location 1 -hide-scrollbar -bw 1 -font "SourceCodeVariable 10")
-
-enable_confirmation=false
-
 # menu defined as an associative array
 typeset -A menu
 
@@ -22,32 +17,11 @@ menu=(
 )
 menu_nrows=${#menu[@]}
 
-# Menu entries that may trigger a confirmation message
-menu_confirm="Shutdown Reboot Logout"
-
-launcher_exe="rofi"
-launcher_options=(-dmenu -i -lines 1 -columns "${menu_nrows}" -p "${ROFI_TEXT}" "${ROFI_OPTIONS[@]}")
-launcher=(${launcher_exe} "${launcher_options[@]}")
+launcher_exe="dmenu -i -p Exit"
+launcher=(${launcher_exe})
 
 selection="$(printf '%s\n' "${!menu[@]}" | sort | "${launcher[@]}")"
 
-function ask_confirmation() {
-  if [ "${launcher_exe}" == "rofi" ]; then
-    confirmed=$(echo -e "Yes\nNo" | rofi -dmenu -i -columns 2 -p "${selection}?" \
-      "${ROFI_OPTIONS[@]}")
-    [ "${confirmed}" == "Yes" ] && confirmed=0
-  fi
-
-  if [ "${confirmed}" == 0 ]; then
-    i3-msg -q "exec ${menu[${selection}]}"
-  fi
-}
-
 if [[ $? -eq 0 && ! -z ${selection} ]]; then
-  if [[ "${enable_confirmation}" = true && \
-        ${menu_confirm} =~ (^|[[:space:]])"${selection}"($|[[:space:]]) ]]; then
-    ask_confirmation
-  else
     i3-msg -q "exec ${menu[${selection}]}"
-  fi
 fi
