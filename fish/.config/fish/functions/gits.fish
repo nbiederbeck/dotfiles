ea7310a3a17671d6723be6001f2af8618e5af0b2
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-12-09 13:31:29 +0100

    shorter aliases

--- fish/.config/fish/functions/gits.fish
+++ fish/.config/fish/functions/gits.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gits --wraps='git status --short $argv' --description 'alias gits git status --short $argv'
+function gits --description 'alias gits git status --short $argv'
   git status --short $argv $argv;
 end
