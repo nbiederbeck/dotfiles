ced175d46ec47b918da2717248d26a1ad76262d2
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-12-18 20:15:09 +0100

    Add paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,6 @@ for conda_exe in /opt/anaconda/bin/conda ~/.local/anaconda3/condabin/conda
         eval $conda_exe "shell.fish" "hook" $argv | source
     end
 end
+
+set PATH $PATH ~/.local/bin
+set PATH $PATH ~/.local/dotbin
