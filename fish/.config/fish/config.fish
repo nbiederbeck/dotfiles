082e7abd85687b6095ce33af2f1a5faa16477570
Noah Biederbeck <nbiederbeck@tutanota.com>
2021-01-04 14:05:30 +0100

    Start X at login

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,17 @@
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
+
+# Anaconda
 for conda_exe in /opt/anaconda/bin/conda ~/.local/anaconda3/condabin/conda
     if test -f $conda_exe
         eval $conda_exe "shell.fish" "hook" $argv | source
     end
 end
 
+# Paths
 set PATH $PATH ~/.local/bin
 set PATH $PATH ~/.local/dotbin
