34e3b1b53866d1f074d07e8b6923c31eb167e65a
Noah Biederbeck <nbiederbeck@tutanota.com>
2021-01-06 13:56:25 +0100

    Nord background

--- fish/.config/fish/functions/feh.fish
+++ fish/.config/fish/functions/feh.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function feh --wraps=feh\ -dF\ -B\'\#fafafa\'\  --description alias\ feh=feh\ -dF\ -B\'\#fafafa\'\ 
- command feh -dF -B'#fafafa'  $argv;
+ command feh -dF -B'#2e3440'  $argv;
 end
