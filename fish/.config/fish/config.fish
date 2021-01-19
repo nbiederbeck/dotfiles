ee179611ce41df675b3db55c0e856b13c0d27cde
Noah Biederbeck <nbiederbeck@tutanota.com>
2021-01-19 09:54:30 +0100

    add sway, fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 # Anaconda
-for conda_exe in /opt/anaconda/bin/conda ~/.local/anaconda3/condabin/conda
+for conda_exe in /opt/anaconda/bin/conda ~/.local/anaconda3/condabin/conda ~/anaconda3/condabin/conda
     if test -f $conda_exe
         eval $conda_exe "shell.fish" "hook" $argv | source
     end
