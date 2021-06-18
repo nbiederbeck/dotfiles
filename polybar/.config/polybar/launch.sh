cf0865217398800e4965e030757654e2cad64141
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2021-06-18 15:16:59 +0200

    cleaner polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,9 +4,6 @@ pkill polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.1; done
 
-# polybar top &!
-# polybar bottom &!
-# polybar combined &!
-polybar bottom_only &!
+polybar bottom &!
 
 echo "Bars launched"
