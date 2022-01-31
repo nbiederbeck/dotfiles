c3bcd98b92ad116608102d778ab365bda9c4ddc6
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-01-31 13:40:53 +0100

    Set PATHS, eval ssh-agent, ignore some history

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,29 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval ~/.local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-conda activate
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
 
-fish_ssh_agent
+if status is-interactive
+    # PATH
+    set -p PATH ~/.local/bin
+    set -p PATH ~/.local/dotbin
+    set -p PATH ~/.cargo/bin
+    set -p PATH ~/.local/texlive/2021/bin/x86_64-linux
+    # ---
+
+    # Conda
+    eval ~/.local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    conda activate
+    # ---
+
+    # ssh-agent
+    eval (ssh-agent -c) > /dev/null
+    # ---
+end
+
+# HISTIGNORE
+function ignorehistory --on-event fish_prompt
+    history delete --exact --case-sensitive 'fg' 'bg'
+end
