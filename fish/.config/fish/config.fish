fd557418ce6295a223063bc8da411477d879a6b5
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-01-31 10:16:27 +0100

    Use tilde

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/nbiederbeck/.local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval ~/.local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 conda activate
 
