585a8a968cad2b89cde02f45a46fe1bdad7b0f6e
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-05-13 09:09:30 +0200

    add minor changes

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,34 +1,42 @@
-require "bootstrap"
+require("bootstrap")
 
 ---@diagnostic disable: undefined-global
 
-return require('packer').startup(function()
-	-- packer can manage itself
-	use { 'wbthomason/packer.nvim' }
-	use { 'morhetz/gruvbox' }
-	use { 'psf/black' }
-	use { 'tpope/vim-commentary' }
+return require("packer").startup({
+	function()
+		-- packer can manage itself
+		use({ "wbthomason/packer.nvim" })
+		use({ "morhetz/gruvbox" })
+		use({ "tpope/vim-commentary" })
 
-	use { 'dense-analysis/ale' }
-	use { 'lervag/vimtex' }
+		use({ "dense-analysis/ale", opt = true })
+		use({ "lervag/vimtex", ft = "tex" })
 
-	-- use {
-	-- 	"williamboman/nvim-lsp-installer",
-	-- 	{
-	-- 		"neovim/nvim-lspconfig",
-	-- 		config = function()
-	-- 			require("nvim-lsp-installer").setup {
-	-- 				ensure_installed = { "sumneko_lua", "jedi_language_server" };
-	-- 				automatic_installation = false,
-	-- 			}
-	-- 			local lspconfig = require("lspconfig")
-	-- 			lspconfig.sumneko_lua.setup {}
-	-- 			lspconfig.jedi_language_server.setup {}
-	-- 		end
-	-- 	}
-	-- }
+		use({ "nvim-lualine/lualine.nvim" })
 
-	if PACKER_BOOTSTRAP then
-		require('packer').sync()
-	end
-end)
+		-- use {
+		-- 	"williamboman/nvim-lsp-installer",
+		-- 	{
+		-- 		"neovim/nvim-lspconfig",
+		-- 		config = function()
+		-- 			require("nvim-lsp-installer").setup {
+		-- 				ensure_installed = { "sumneko_lua", "jedi_language_server" };
+		-- 				automatic_installation = false,
+		-- 			}
+		-- 			local lspconfig = require("lspconfig")
+		-- 			lspconfig.sumneko_lua.setup {}
+		-- 			lspconfig.jedi_language_server.setup {}
+		-- 		end
+		-- 	}
+		-- }
+
+		if PACKER_BOOTSTRAP then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		profile = {
+			enable = true,
+		},
+	},
+})
