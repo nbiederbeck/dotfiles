6ad0a0cc881e00617542bb9d7c997072b2b1453b
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-09-13 10:04:30 +0200

    Lint on BufEnter

--- nvim/.config/nvim/lua/user/linter.lua
+++ nvim/.config/nvim/lua/user/linter.lua
@@ -15,7 +15,7 @@ lint.linters_by_ft = {
 }
 
 -- Lint on save
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
 	callback = function()
 		lint.try_lint()
 	end,
