faf096ee50746b3ddc66e976800a96f014083efd
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-09-13 08:45:03 +0200

    add flake8 options

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -25,8 +25,13 @@ require("formatter").setup({
 	},
 })
 
+local flake8 = require("lint.linters.flake8")
+flake8.args = {
+	"--extend-ignore=E402",
+	"--max-line-length=88",
+}
 require("lint").linters_by_ft = {
-	python = { "flake8" },
+	python = { flake8 },
 	tex = { "chktex" },
 	sh = { "shellcheck" },
 }
