e159215af005539153e0087633f4b6035d6b3f8f
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-02-01 11:28:36 +0100

    reintroduce formatter/linter options

--- nvim/.config/nvim/lua/custom/ale.lua
+++ nvim/.config/nvim/lua/custom/ale.lua
@@ -3,12 +3,18 @@ local g = vim.g
 g.ale_use_neovim_diagnostics_api = 1
 g.ale_lint_on_text_changed = 1
 g.ale_fix_on_save = 1
+
+-- Linters
 g.ale_linters = {
 	python = { "flake8" },
 	tex = { "chktex" },
 	sh = { "shellcheck" },
 	zsh = { "shellcheck" },
 }
+-- Linter Options
+g.ale_python_flake8_options = "--max-line-length=88 --extend-ifnore=E402"
+
+-- Fixers
 g.ale_fixers = {
 	python = { "isort", "black" },
 	lua = { "stylua" },
@@ -18,3 +24,6 @@ g.ale_fixers = {
 	snakemake = { "snakefmt" },
 	["*"] = { "remove_trailing_lines", "trim_whitespace" },
 }
+-- Fixer options
+g.ale_sh_shfmt_options = "-i 4"
+g.ale_python_isort_options = "--profile=black"
