9e6ba575d4923301b34f2b1429557be6f3ed7af6
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-03-09 16:16:09 +0100

    showbreak and listchars

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,3 +45,7 @@ map("n", "ga", "<Plug>(EasyAlign)", {})
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+vim.opt.showbreak = "⇒"
+vim.opt.listchars = "tab:>-"
+vim.opt.list = true
